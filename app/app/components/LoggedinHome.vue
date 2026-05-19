06711c563c88128a5571327e1000e3cd88f2607c
Edward-Mercado <edwardmercado149@gmail.com>
2026-05-19 12:10:40 -0400

    as it is

--- app/app/components/LoggedinHome.vue
+++ app/app/components/LoggedinHome.vue
@@ -6,7 +6,6 @@
             <MenuButton :url="'/inventory'" :title="'Inventory'"></MenuButton>
             <MenuButton :url="'/junkyard-gacha'" :title="'Gacha'"></MenuButton>
             <MenuButton :url="'/trading'" :title="'Trading'"></MenuButton>
-            <MenuButton :url="'/profile-settings'" :title="'Profile Settings'"></MenuButton>
             <MenuButton :url="'/money-game'" :title="'Get Money'"></MenuButton>
         </div>
         <div class="lg:w-[70%] lg:block hidden h-full text-black bg-slate-200 pt-[20%]">
