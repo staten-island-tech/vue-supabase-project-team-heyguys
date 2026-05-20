0fe1a6faf04d3591379b3e3d9c5a2dc743b62df4
humd1 <humdb@nycstudents.net>
2026-05-20 10:03:33 -0400

    basic styling

---
+++ app/app/components/FilterButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <button class="bg-white border border-gray-200 press-start text-sky-300 h-3px px-2 flex items-center justify-center rounded-lg text-[0.63rem] text-center
+            transition-all ease-in-out duration-300 hover:bg-yellow-200/95 hover:text-yellow-500 hover:-translate-y-1 hover:border-yellow-500 hover:border" 
+            :class="h"
+            >
+        <slot></slot>
+    </button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    h: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/app/components/FilterMenu.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import FilterButton from './FilterButton.vue'
+
+defineProps<{
+  filters: string[]
+}>()
+</script>
+
+<template>
+  <div class="flex gap-1">
+    <FilterButton
+      v-for="filter in filters"
+      :key="filter"
+      h="h-10"
+    >
+      {{ filter }}
+    </FilterButton>
+  </div>
+</template>
