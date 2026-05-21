9a00d0068ccfa0bcb114419ed6c2ea0b7e8202b0
Edward-Mercado <edwardmercado149@gmail.com>
2026-05-20 20:47:19 -0400

    basic layout of gacha page

---
+++ app/app/components/ResultsPage.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="carousel rounded-box bg-white w-full h-[68%] p-[1%] flex flex-row items-center justify-center">
+        <div class="carousel-item w-[30%] h-full bg-black mx-[2%]" v-for="result in results">
+            {{ result }}
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    results: {
+        type: Array,
+        required: true
+    }
+})
+
+</script>
+
+<style scoped>
+.result-page-enter-from {
+    opacity: 0;
+    transform: translateX(200%)
+}
+.result-page-enter-active {
+    transition: all 0.5s ease-out;
+}
+.result-page-enter-to {
+    opacity: 1;
+    transform: translateX(0%)
+}
+</style>
--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
-  
+  ssr: false,
   modules: ['@nuxt/eslint', '@pinia/nuxt', '@nuxtjs/tailwindcss', '@nuxt/icon'],
   runtimeConfig: {
     public: {
