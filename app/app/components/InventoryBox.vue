a9bb60e24e5904ec83399251975e394f0b7c4f0c
humd1 <humdb@nycstudents.net>
2026-05-21 10:29:53 -0400

    infoboxes

--- app/app/components/InventoryBox.vue
+++ app/app/components/InventoryBox.vue
@@ -1,10 +1,17 @@
 <template>
-    <div class="InventoryBox relative bg-white h-[125px] w-[125px] border border-yellow-500 press-start text-sky-300 px-2 flex items-center flex-row justify-center rounded-lg text-[0.63rem] text-left
+    <div class="InventoryBox group relative bg-white h-[125px] w-[125px] border border-yellow-500 press-start text-sky-300 px-2 flex items-center flex-row justify-center rounded-lg text-[0.63rem] text-left
             transition-all ease-in-out duration-300 hover:bg-gray-100/95 hover:text-yellow-500 hover:-translate-y-1 hover:border-yellow-500 hover:border" 
             >
         <img src="/spriteplaceholder.PNG" alt="blah" class="aspect-square w-12 h-12 object-contain">
         <div class="flex mt-2 ml-2">
             <h3 class="text-[0.63rem] text-gray-800 absolute bottom-1 left-1">1x</h3>
+        </div>
+        <div class= "hidden group-hover:block">
+        <InfoBox>
+            <p>name</p>
+            <p>type</p>
+            <p>desc</p>
+        </InfoBox>
         </div>
     </div>
 </template>
--- app/app/pages/inventory.vue
+++ app/app/pages/inventory.vue
@@ -15,8 +15,8 @@
                     <InventoryBox v-for="i in 50" :key="i" /> <!--REPLACE THIS WITH USER INVENTORY LENGTH LATER-->
                 </div>
             </div>
-            <div class= "flex flex-col justify-end items-start mr-[200px] ml-auto mb-[-10]">
-                <button class="bg-white flex border-yellow-500 border-2 text-2xl rounded-lg press-start text-yellow-500 h-[10%] w-full px-5 py-2 pr-10 pl-10 mt-[30px]
+            <div class= "flex flex-col justify-end items-start mr-[200px] ml-auto mb-[-10] w-300px">
+                <button class="bg-white flex border-yellow-500 border-2 text-2xl rounded-lg press-start text-yellow-500 h-min w-full px-5 py-2 pr-10 pl-10 mt-[30px]
                 transition-all ease-in-out hover:bg-yellow-200 hover:-translate-y-[2%] active:translate-y-[2%] active:bg-yellow-300" @click="$emit('buildmode')">Build New</button>
                 <div class ="h-full w-full items-center bg-gray-300 mt-1 rounded-xl">
                 </div>
