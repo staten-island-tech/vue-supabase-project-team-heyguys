e7fac275819e95244fa2167c4dc4591a951a70f1
humd1 <humdb@nycstudents.net>
2026-05-21 10:57:58 -0400

    a bunch of buttons

--- app/app/components/InventoryBox.vue
+++ app/app/components/InventoryBox.vue
@@ -2,7 +2,7 @@
     <div class="InventoryBox group relative bg-white h-[125px] w-[125px] border border-yellow-500 press-start text-sky-300 px-2 flex items-center flex-row justify-center rounded-lg text-[0.63rem] text-left
             transition-all ease-in-out duration-300 hover:bg-gray-100/95 hover:text-yellow-500 hover:-translate-y-1 hover:border-yellow-500 hover:border" 
             >
-        <img src="/spriteplaceholder.PNG" alt="blah" class="aspect-square w-12 h-12 object-contain">
+        <img src="/spriteplaceholder.PNG" alt="insert sprite" class="flex aspect-square w-12 h-12 object-contain">
         <div class="flex mt-2 ml-2">
             <h3 class="text-[0.63rem] text-gray-800 absolute bottom-1 left-1">1x</h3>
         </div>
@@ -12,6 +12,9 @@
             <p>type</p>
             <p>desc</p>
         </InfoBox>
+        </div>
+        <div class="absolute bottom-1 right-1">
+            <MiniButton type="add"></MiniButton> 
         </div>
     </div>
 </template>
--- app/app/components/MiniButton.vue
+++ app/app/components/MiniButton.vue
@@ -1,17 +1,33 @@
 <template>
-    <button class="aspect-square bg-green-400 border-emerald-900 border-2 w-[20%] rounded-xl
-                hover:bg-green-500 active:bg-green-700 font-black text-xl
-                ease-in-out duration-300 hover:-translate-y-[2%] active:translate-y-[2%]
-                "> + </button>
-                <button class="aspect-square bg-red-400 border-2 border-red-900 w-[20%] rounded-xl font-black text-xl
-                hover:bg-red-500 active:bg-red-700
-                ease-in-out duration-300 hover:-translate-y-[2%] active:translate-y-[2%]
-                "> - </button>
-
+    <button class="aspect-square border-2 w-[full] rounded-lg px-1 font-black text-xs text-white
+                ease-in-out duration-300 hover:-translate-y-[2%] active:translate-y-[2%]" 
+                :class="buttonClasses"> 
+                {{ symbol }} </button>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
 
+const props = defineProps<{
+  type: 'add' | 'remove' | 'edit' | 'delete'
+}>()
+
+const symbol = computed(() => {
+  return props.type === 'add' ? '+' : props.type === 'remove' ? '-' : props.type === 'edit' ? '🛠' : '🗑'
+})
+
+const buttonClasses = computed(() => {
+  if (props.type === 'add'){
+    return 'bg-green-300 border-green-700 hover:bg-green-400 hover:border-green-800'
+  }
+  if (props.type === 'remove'){
+    return 'bg-red-300 border-red-700 hover:bg-red-400 hover:border-red-800'
+  }
+  if (props.type === 'delete'){
+    return 'bg-red-300 border-red-700 hover:bg-red-400 hover:border-red-800'
+  }
+  return 'bg-green-300 border-green-700 hover:bg-green-400 hover:border-green-800'
+})
 </script>
 
 <style scoped>
