1d46ef65852347df38624063b93388db4127df4f
humd1 <humdb@nycstudents.net>
2026-05-26 12:14:41 -0400

    Update InventoryStore.ts

--- app/app/stores/InventoryStore.ts
+++ app/app/stores/InventoryStore.ts
@@ -1,6 +1,15 @@
 import { defineStore } from 'pinia'
 import {supabase} from '../lib/supabaseClient'
 
+export interface RobotPart {
+  id: string     
+  part_id: string 
+  user_id: string
+  completed_robot_id: string | null
+  quantity: number
+}
+
+
 export const useInventoryStore = defineStore('owned_robot_parts', {
     state: () => ({
         inventory: [] as any[],
