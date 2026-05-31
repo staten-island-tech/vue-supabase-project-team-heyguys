cbc1d8fa3cb397b918d6a5f3082999575bf4a0d7
Edward-Mercado <edwardmercado149@gmail.com>
2026-05-31 13:12:34 -0400

    the game works

---
+++ app/app/components/play/GameIdle.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="w-full h-full justify-around flex flex-col">
+        <h3 class="text-2xl text-center justify-start text-yellow-500 press-start mt-12 mb-12"> YOUR PREVIOUS SCORE: {{ score }}</h3>
+        <GeneralButton :w="'w-full'" :h="'max-h-[20%]'" @click="$emit('play')">PLAY!</GeneralButton>
+    </div>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['play'])
+defineProps({
+    score: {
+        type: [String, Number],
+        required: true
+    },
+})
+</script>
+
+<style scoped>
+
+</style>
--- app/app/pages/money-game.vue
+++ app/app/pages/money-game.vue
@@ -6,15 +6,17 @@
         </div>
         <div class="flex flex-col items-center bg-slate-300/60 z-10 w-[80%] h-[80%] rounded-3xl border-yellow-500 border-2 p-[1%]">
             <h2 class="text-4xl justify-start text-yellow-500 press-start text-left"> TENTATIVE MADNESS </h2>
-            <h3 class="text-2xl text-center justify-start text-yellow-200 press-start mt-12 mb-12"> To play, press [SPACE] when the bars match up!</h3>
-            <play-game-bar></play-game-bar>
+            <h3 class="text-2xl text-center justify-start text-yellow-200 press-start mt-12 mb-12"> To play, press the button when the bars match up!</h3>
+            <play-game-bar v-if="playing" @lose="(playedScore) => {playing = false; score = playedScore}"></play-game-bar>
+            <play-game-idle v-else @play="playing = true" :score="score"></play-game-idle>
         </div>
         
         </div>
 </template>
 
 <script setup lang="ts">
-
+const score = ref<number | string>("N/A")
+const playing = ref<boolean>(false)
 </script>
 
 <style scoped>
