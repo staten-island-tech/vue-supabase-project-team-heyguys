e34bd75487af928895482d81ac24594d12a00590
Edward-Mercado <edwardmercado149@gmail.com>
2026-05-31 21:15:23 -0400

    you are logged in

--- app/app/components/TopBar.vue
+++ app/app/components/TopBar.vue
@@ -7,11 +7,21 @@
         <GeneralButton :w="'w-[20%]'" :h="'max-h-[60%] mx-[2%]'"
         @click="useLoginStore().logOut"
         >LOG OUT</GeneralButton>
+
+        <div v-if="loginStore.loggedInUser" role="alert" class="alert alert-info bg-sky-300/50 absolute bottom-[-25%] left-[0.5%] z-10 h-[7%] flex">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="h-6 w-6 shrink-0 stroke-current text-black">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+            </svg>
+
+            <span class="text-bold press-start text-xs text-black italic">
+                You are logged in as {{ loginStore.loggedInUser?.email }}
+            </span>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-
+const loginStore = useLoginStore()
 </script>
 
 <style scoped>
