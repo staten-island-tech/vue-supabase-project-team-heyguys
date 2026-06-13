55579483faeabe072307f81aaec7cd551b009ef2
humd1 <humdb@nycstudents.net>
2026-06-13 11:35:23 -0400

    not sure where this was from

--- app/app/components/InventoryBox.vue
+++ app/app/components/InventoryBox.vue
@@ -14,12 +14,16 @@
         </InfoBox>
         </div>
         <div class="absolute bottom-1 right-1">
-            <MiniButton type="add"></MiniButton> <!-- this needs to be a v-if depending on whether build mode is on or not later!! -->
+                <MiniButton type="add"></MiniButton>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+
+function handleBuildMode(){
+    console.log('build mode')
+}
 
 </script>
 
