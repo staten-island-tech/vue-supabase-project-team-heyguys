613e231f87114e16f1a99c2de35a89d8e097f553
humd1 <humdb@nycstudents.net>
2026-06-14 15:29:14 -0400

    this is gonna look so dumb lol

--- app/app/components/inventory/RobotSprite.vue
+++ app/app/components/inventory/RobotSprite.vue
@@ -1,39 +1,40 @@
 <template>
-  <div class="relative h-[60px] w-[60px]">
+  <div class="relative h-[80px] w-[60px]">
+
+    <img
+      v-if="parts.Body" :src="parts.Body.itemInfo.sprite_url"
+      alt="Body"
+      class="absolute left-1/2 top-[10px] h-[42px] w-[40px] -translate-x-1/2 object-contain"
+    >
+
     <img
       v-if="parts.Head" :src="parts.Head.itemInfo.sprite_url"
       alt="Head"
-      class="absolute left-1/2 top-0 h-[30px] w-[42px] -translate-x-1/2 object-contain"
+      class="absolute left-1/2 top-0 h-[20px] w-[20px] -translate-x-1/2 object-contain"
     >
 
     <img
       v-if="parts['Left Arm']" :src="parts['Left Arm'].itemInfo.sprite_url"
       alt="Left arm"
-      class="absolute left-[5px] top-[27px] h-[38px] w-[18px] object-contain"
-    >
-
-    <img
-      v-if="parts.Body" :src="parts.Body.itemInfo.sprite_url"
-      alt="Body"
-      class="absolute left-1/2 top-[25px] h-[42px] w-[40px] -translate-x-1/2 object-contain"
+      class="absolute left-[10px] top-[10px] h-[38px] w-[18px] object-contain"
     >
 
     <img
       v-if="parts['Right Arm']" :src="parts['Right Arm'].itemInfo.sprite_url"
       alt="Right arm"
-      class="absolute right-[5px] top-[27px] h-[38px] w-[18px] object-contain"
+      class="absolute right-[10px] top-[10px] h-[38px] w-[18px] object-contain"
     >
 
     <img
       v-if="parts['Left Leg']" :src="parts['Left Leg'].itemInfo.sprite_url"
       alt="Left leg"
-      class="absolute bottom-0 left-[24px] h-[32px] w-[18px] object-contain"
+      class="absolute bottom-0 left-[24px] h-[50px] w-[20px] object-contain"
     >
 
     <img
       v-if="parts['Right Leg']" :src="parts['Right Leg'].itemInfo.sprite_url"
       alt="Right leg"
-      class="absolute bottom-0 right-[24px] h-[32px] w-[18px] object-contain"
+      class="absolute bottom-0 right-[24px] h-[50px] w-[20px] object-contain"
     >
   </div>
 </template>
